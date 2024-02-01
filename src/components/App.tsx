e9a8cb46f1b1e7e261337c69677a35b92410c8fb
VSebastian8 <emanuel-sebastian.visan-draghicescu@s.unibuc.ro>
2024-02-01 16:32:12 +0200

    Deploy Try

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Board from "./Board";
-import { Button, Grid, Typography } from "@material-ui/core";
+// import { Button, Grid, Typography } from "@material-ui/core";
 import { useState } from "react";
 
 function App() {
@@ -24,20 +24,9 @@ function App() {
   };
 
   return (
-    <Grid
-      container
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      spacing={2}
-    >
-      <Grid item xs={12}>
-        <Typography variant="h6" component="h6">
-          Revealed: {revealed} / {rows * columns - bombs}
-        </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <Board
+    <>
+    <h5> Revealed: {revealed} / {rows * columns - bombs}</h5>
+     <Board
           rows={rows}
           columns={columns}
           bombNumber={bombs}
@@ -46,18 +35,43 @@ function App() {
           retry={retried}
           update={handleReveal}
         />
-      </Grid>
-      <Grid item xs={12}>
-        <Button color="secondary" variant="contained" onClick={handleRetry}>
-          Retry
-        </Button>
-      </Grid>
-      <Grid item xs={12}>
-        <Button color="primary" variant="contained" onClick={handleRefresh}>
-          Refresh Board
-        </Button>
-      </Grid>
-    </Grid>
+        <button onClick={handleRetry}>Retry</button>
+        <button onClick={handleRefresh}>Refresh</button>
+    </>
+    // <Grid
+    //   container
+    //   direction="column"
+    //   alignItems="center"
+    //   justifyContent="center"
+    //   spacing={2}
+    // >
+    //   <Grid item xs={12}>
+    //     <Typography variant="h6" component="h6">
+    //       Revealed: {revealed} / {rows * columns - bombs}
+    //     </Typography>
+    //   </Grid>
+    //   <Grid item xs={12}>
+    //     <Board
+    //       rows={rows}
+    //       columns={columns}
+    //       bombNumber={bombs}
+    //       squareSize={30}
+    //       refresh={refreshed}
+    //       retry={retried}
+    //       update={handleReveal}
+    //     />
+    //   </Grid>
+    //   <Grid item xs={12}>
+    //     <Button color="secondary" variant="contained" onClick={handleRetry}>
+    //       Retry
+    //     </Button>
+    //   </Grid>
+    //   <Grid item xs={12}>
+    //     <Button color="primary" variant="contained" onClick={handleRefresh}>
+    //       Refresh Board
+    //     </Button>
+    //   </Grid>
+    // </Grid>
   );
 }
 
