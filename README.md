087ae267bb773fa0cc71a5d33337744bf44095f3
VSebastian8 <emanuel-sebastian.visan-draghicescu@s.unibuc.ro>
2024-02-01 18:38:01 +0200

    Styling

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # MineSweeper
- React + Typescript mini project
+
+> React + Typescript mini project
+
+**Website hosted at: https://react-minesweeper-uqg7.onrender.com/**
+
+![Mines](./customBoard.png "Tile Colors")
+
+---
+
+**Features**
+
+- Left click to reveal tile
+- Right click to lock/unlock tile
+- Retry button keeps the same mines
+- Refresh button restarts the game
+
+<br>
+
+**Resources:**
+
+- Material UI: https://mui.com/material-ui/
+- Sound effects: https://www.zapsplat.com/
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,32 @@
 import "./App.css";
 import Board from "./Board";
-// import { Button, Grid, Typography } from "@material-ui/core";
 import { useState } from "react";
-import { Button, Grid, Typography } from "@mui/material";
+import {
+  Button,
+  Grid,
+  Typography,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#14b8a6",
+    },
+    secondary: {
+      main: "#f43f5e",
+    },
+  },
+});
 
 function App() {
   const [refreshed, setRefreshed] = useState(false);
   const [retried, setRetried] = useState(false);
   const [revealed, setRevealed] = useState(0);
-  const bombs = 20;
-  const rows = 10;
-  const columns = 15;
+  const bombs = 25;
+  const rows = 8;
+  const columns = 14;
 
   const handleRefresh = () => {
     setRefreshed(!refreshed);
@@ -25,40 +41,42 @@ function App() {
   };
 
   return (
-    <Grid
-      container
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      spacing={2}
-    >
-      <Grid item xs={12}>
-        <Typography variant="h6" component="h6">
-          Revealed: {revealed} / {rows * columns - bombs}
-        </Typography>
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        spacing={2}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h6" component="h6">
+            Revealed: {revealed} / {rows * columns - bombs}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Board
+            rows={rows}
+            columns={columns}
+            bombNumber={bombs}
+            squareSize={30}
+            refresh={refreshed}
+            retry={retried}
+            update={handleReveal}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button color="secondary" variant="contained" onClick={handleRetry}>
+            Retry
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button color="primary" variant="contained" onClick={handleRefresh}>
+            Refresh
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Board
-          rows={rows}
-          columns={columns}
-          bombNumber={bombs}
-          squareSize={30}
-          refresh={refreshed}
-          retry={retried}
-          update={handleReveal}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Button color="secondary" variant="contained" onClick={handleRetry}>
-          Retry
-        </Button>
-      </Grid>
-      <Grid item xs={12}>
-        <Button color="primary" variant="contained" onClick={handleRefresh}>
-          Refresh Board
-        </Button>
-      </Grid>
-    </Grid>
+    </ThemeProvider>
   );
 }
 
