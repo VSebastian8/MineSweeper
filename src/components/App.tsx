d6d95726e5c84924b68bb1e3465de5f108977a67
VSebastian8 <emanuel-sebastian.visan-draghicescu@s.unibuc.ro>
2024-02-04 00:32:56 +0200

    Centering

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -42,40 +42,43 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        spacing={2}
-      >
-        <Grid item xs={12}>
-          <Typography variant="h6" component="h6">
-            Revealed: {revealed} / {rows * columns - bombs}
-          </Typography>
+      <div id="container">
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          spacing={2}
+        >
+          <Grid item xs={12}>
+            <Typography variant="h6" component="h6">
+              Revealed: {revealed} / {rows * columns - bombs}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Board
+              rows={rows}
+              columns={columns}
+              bombNumber={bombs}
+              squareSize={30}
+              refresh={refreshed}
+              retry={retried}
+              update={handleReveal}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button color="secondary" variant="contained" onClick={handleRetry}>
+              Retry
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button color="primary" variant="contained" onClick={handleRefresh}>
+              Refresh
+            </Button>
+          </Grid>
+          <Grid item xs={12}></Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Board
-            rows={rows}
-            columns={columns}
-            bombNumber={bombs}
-            squareSize={30}
-            refresh={refreshed}
-            retry={retried}
-            update={handleReveal}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button color="secondary" variant="contained" onClick={handleRetry}>
-            Retry
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Button color="primary" variant="contained" onClick={handleRefresh}>
-            Refresh
-          </Button>
-        </Grid>
-      </Grid>
+      </div>
     </ThemeProvider>
   );
 }
